962447b1b863f8ee54c0dc631716485c3660662a
Nelson Mbau <mbau.ndungu@gmail.com>
2024-03-20 19:13:53 +0300

    feat: add necessary functions, in various ways

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
 // code your solution here
+function saturdayFun(activity = 'roller-skate') {
+  return `This Saturday, I want to ${activity}!`
+}
+
+const mondayWork = function mondayWork(activity = 'go to the office') {
+  return `This Monday, I will ${activity}.`
+}
+
+const wrapAdjective = (wrapper = "*") => (adjective) => `You are ${wrapper}${adjective}${wrapper}!`
